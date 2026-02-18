3e6a71426c81144f77dae45629bc22a322ed4574
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2026-02-18 13:50:51 +0100

    feat: add zod schema

---
+++ schema/index.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  name: z.string().min(1, "Product name is required"),
+  category: z.enum(["headphones", "earphones", "speakers"], "Category is required"),  
+  description: z.string().min(10, "Description must be at least 10 characters"),
+  isFeatured: z.boolean().default(false),
+});
+
+export type ProductInput = z.infer<typeof productSchema>;
+
+export type ProductErrors = z.core.$ZodFlattenedError<ProductInput>["fieldErrors"]
