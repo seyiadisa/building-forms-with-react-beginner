ca6e5eae5e669957f8911f49e0be5db6389f9f06
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2026-02-18 14:18:40 +0100

    feat: add validation for specifications

--- actions/index.ts
+++ actions/index.ts
@@ -1,37 +1,35 @@
 "use server";
 
 import { productSchema, ProductInput, ProductErrors } from "@/schema";
-import { z } from 'zod';
+import { z } from "zod";
 
 type ActionState = {
-  data?: ProductInput
-  errors?: ProductErrors
-  success?: boolean
-}
+  data?: ProductInput;
+  errors?: ProductErrors;
+  success?: boolean;
+};
 
 export async function createProduct(
-    _prevState: ActionState,
-    formData: FormData
-  ): Promise<ActionState> {
-    // convert the FormData to a JS object 
-    const data = Object.fromEntries(formData.entries());
+  _prevState: ActionState,
+  formData: FormData,
+): Promise<ActionState> {
+  const data = Object.fromEntries(formData.entries());
+  data["specifications"] = JSON.parse(formData.get("specifications") as string);
+  const validatedData = productSchema.safeParse(data);
 
-    const validatedData = productSchema.safeParse(data);
+  if (!validatedData.success) {
+    // Map Zod errors to { fieldName: message }
+    const errors = z.treeifyError(validatedData.error).properties;
 
-    // if you prefer not to use zod, this is where you would
-    // implement your validation logic
-    if (!validatedData.success) {
-      const errors = z.flattenError(validatedData.error).fieldErrors;
-      return {
-        errors,
-        data: data as ProductInput 
-      };
-    }
-
-    // Here you can perform actions such as saving to a DB
-    // or sending a request to an API
-      
     return {
-      success: true,
+      errors,
+      data: data as unknown as ProductInput,
     };
- } 
+  }
+
+  // Here you can perform actions such as saving to a DB
+
+  return {
+    success: true,
+  };
+}
