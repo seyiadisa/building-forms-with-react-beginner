2189caf9cf3d3be72d88e9b21d9f6d840b72e840
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2026-02-18 14:18:26 +0100

    feat: add specifications field in schema

--- schema/index.ts
+++ schema/index.ts
@@ -2,11 +2,23 @@ import { z } from "zod";
 
 export const productSchema = z.object({
   name: z.string().min(1, "Product name is required"),
-  category: z.enum(["headphones", "earphones", "speakers"], "Category is required"),  
+  category: z.enum(
+    ["headphones", "earphones", "speakers"],
+    "Category is required",
+  ),
   description: z.string().min(10, "Description must be at least 10 characters"),
   isFeatured: z.string().optional(),
+  specifications: z
+    .array(
+      z.object({
+        id: z.number(),
+        label: z.string().min(1, "Label is required"),
+        value: z.string().min(1, "Value is required"),
+      }),
+    )
+    .min(1, "Enter at least one specification"),
 });
 
 export type ProductInput = z.infer<typeof productSchema>;
 
-export type ProductErrors = z.core.$ZodFlattenedError<ProductInput>["fieldErrors"]
+export type ProductErrors = z.core.$ZodErrorTree<ProductInput>["properties"];
